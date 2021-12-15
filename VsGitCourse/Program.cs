72be92c52163844bed8ce2b5b20f45c16286c5a0
AnatolFaraj <96137951+AnatolFaraj@users.noreply.github.com>
2021-12-15 19:49:01 +0300

    Add files via upload

---
+++ VsGitCourse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VsGitCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+
+            Console.WriteLine("not hi anyone, you suck");
+
+            
+
+        }
+    }
+}
